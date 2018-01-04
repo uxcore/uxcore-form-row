d4081bb979df9cc1ae3b7970fdd76dd63afee8cd
光弘 <guanghong.wsj@alibaba-inc.com>
2018-01-04 21:57:37 +0800

    test coverage to 100%

---
+++ tests/FormRow.spec.jsx
@@ -0,0 +1,23 @@
+import expect from 'expect.js';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import FormField from 'uxcore-form-field';
+
+import FormRow from '../src';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('FormRow', () => {
+  it('should render correctly', () => {
+    mount(<FormRow />);
+  });
+  it('should process child correctly', () => {
+    const wrapper = mount(
+      <FormRow data={{}} attachFormField={() => {}} handleDataChange={() => {}}>
+        <FormField />
+      </FormRow>
+    );
+    expect(wrapper.find(FormField).length).not.to.be(0);
+  });
+});
